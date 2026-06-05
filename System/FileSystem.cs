e3bc7ae44ad74311ff4fe61f4061b56ea4224559
thatben <thatbenbierens@gmail.com>
2026-06-05 16:35:39 +0200

    Implements filesystem

--- System/FileSystem.cs
+++ System/FileSystem.cs
@@ -1,4 +1,6 @@
 ﻿using ContentTower.Services;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
 
 namespace ContentTower.System
 {
@@ -17,63 +19,99 @@ namespace ContentTower.System
 
     public class FileSystem : IFileSystem
     {
-        public bool CheckCreateDir(string dataPath)
+        private readonly StorageOptions options;
+        private readonly ILogger<FileSystem> logger;
+
+        public FileSystem(ILogger<FileSystem> logger, IOptions<StorageOptions> options)
         {
-            if (!Directory.Exists(options.DataPath))
-            {
-                Directory.CreateDirectory(options.DataPath);
-                if (!Directory.Exists(options.DataPath))
-            }
+            this.options = options.Value;
+            this.logger = logger;
         }
 
-        public Task DeleteData(Cid cid)
+        public bool CheckCreateDir(string path)
         {
-            throw new NotImplementedException();
+            if (Directory.Exists(path)) return true;
+            Directory.CreateDirectory(path);
+            return Directory.Exists(path);
         }
 
-        public Task DeleteObject(Cid cid)
+        public async Task DeleteData(Cid cid)
         {
-            throw new NotImplementedException();
+            File.Delete(GetDataFilepath(cid));
+        }
+
+        public async Task DeleteObject(Cid cid)
+        {
+            File.Delete(GetJsonFilepath(cid));
         }
 
         public bool Exists(Cid cid)
         {
-            throw new NotImplementedException();
+            return
+                File.Exists(GetJsonFilepath(cid)) &&
+                File.Exists(GetDataFilepath(cid));
         }
 
-        public Task IterateObjects<T>(Action<T> onObject)
+        public async Task IterateObjects<T>(Action<T> onObject)
         {
-            throw new NotImplementedException();
+            var files = Directory.GetFiles(options.DataPath);
+            foreach (var file in files)
+            {
+                if (file.ToLowerInvariant().EndsWith(".json"))
+                {
+                    TryJson(file, onObject);
+                }
+            }
         }
 
-        public Task<Stream> ReadData(Cid cid)
+        public async Task<Stream> ReadData(Cid cid)
         {
-            throw new NotImplementedException();
+            return File.OpenRead(GetDataFilepath(cid));
         }
 
-        public Task<T> ReadObject<T>(Cid cid)
+        public async Task<T> ReadObject<T>(Cid cid)
         {
-            throw new NotImplementedException();
+            var obj = GetJson<T>(GetJsonFilepath(cid));
+            if (obj == null) throw new Exception("Failed to load object for " + cid);
+            return obj;
         }
 
-        public Task WriteData(string name, byte[] data)
+        public async Task WriteData(Cid cid, byte[] data)
         {
-            throw new NotImplementedException();
+            File.WriteAllBytes(GetDataFilepath(cid), data);
         }
 
-        public Task WriteData(Cid cid, byte[] data)
+        public async Task WriteObject<T>(Cid cid, T obj)
         {
-            throw new NotImplementedException();
+            File.WriteAllText(GetJsonFilepath(cid), JsonConvert.SerializeObject(obj));
         }
 
-        public Task WriteObject<T>(string name, T obj)
+        private void TryJson<T>(string file, Action<T> onObject)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var obj = GetJson<T>(file);
+                if (obj != null) onObject(obj);
+            }
+            catch (Exception ex)
+            {
+                logger.LogWarning(ex, "Exception in filesystem abstraction");
+            }
         }
 
-        public Task WriteObject<T>(Cid cid, T obj)
+        private T? GetJson<T>(string file)
         {
-            throw new NotImplementedException();
+            return JsonConvert.DeserializeObject<T>(File.ReadAllText(file));
+        }
+
+        private string GetJsonFilepath(Cid cid)
+        {
+            return Path.Combine(options.DataPath, cid.Hash + ".json");
+        }
+
+        private string GetDataFilepath(Cid cid)
+        {
+            return Path.Combine(options.DataPath, cid.Hash + ".data");
         }
     }
 }
