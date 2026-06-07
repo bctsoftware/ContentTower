a3788a7c0e1ba9476f54a9d15cf3b51aaf0605c3
thatben <thatbenbierens@gmail.com>
2026-06-07 19:40:33 +0200

    setting up integration tests

--- ContentTower/Controllers/QuotaController.cs
+++ ContentTower/Controllers/QuotaController.cs
@@ -1,5 +1,6 @@
 ﻿using ContentTower.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 
 namespace ContentTower.Controllers
 {
@@ -8,16 +9,25 @@ namespace ContentTower.Controllers
     public class QuotaController : ControllerBase
     {
         private readonly IQuotaService quotaService;
+        private readonly StorageOptions options;
 
-        public QuotaController(IQuotaService quotaService)
+        public QuotaController(IQuotaService quotaService, IOptions<StorageOptions> options)
         {
             this.quotaService = quotaService;
+            this.options = options.Value;
         }
 
         [HttpGet]
         public async Task<QuotaResponse> Get()
         {
             return quotaService.GetQuotaStatus();
+        }
+
+        [HttpGet]
+        [Route("interval")]
+        public async Task<IActionResult> GetInterval()
+        {
+            return Ok(options.CleanupIntervalSeconds);
         }
     }
 
